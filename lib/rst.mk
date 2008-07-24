0857f033b6350fc855f839f488bc0eed7bf7d2b1
Matthew Leingang <leingang@nyu.edu>
2008-07-24 15:59:14 +0000

    Added a -screen directive to wrap pages in CIMS templates

--- lib/rst.mk
+++ lib/rst.mk
@@ -9,6 +9,8 @@ RST2HTML=$(TOPDIR)/bin/rst2html.py
 RST2HTML_FLAGS=--embed-stylesheet \
 	--stylesheet-path=$(LIBDIR)/new.css \
 	--cloak-email-addresses
+PYTHON=python
+DUDIR=$(LIBDIR)/docutils-0.5
 RST2LATEX=$(PYTHON) $(DUDIR)/bin/rst2latex.py
 XSLTPROC=xsltproc
 XSLTPROC_ISITES_FLAGS=
@@ -17,7 +19,7 @@ $(RST2HTML): $(DUDIR)/tools/rst2html.py
 	cd $(DUDIR) && $(PYTHON) setup.py install --home=$(DUDIR);\
 	echo "#!/usr/bin/env python\n\
 import sys\n\
-sys.path.append(\"$(shell cd $(DUDIR) && pwd)/lib/python\")" > $(RST2HTML); \
+sys.path.append('$(shell cd $(DUDIR) && pwd)/lib/python')" > $(RST2HTML); \
 	cat $(DUDIR)/tools/rst2html.py >> $(RST2HTML)
 	chmod 701 $(RST2HTML)
 
@@ -26,6 +28,9 @@ sys.path.append(\"$(shell cd $(DUDIR) && pwd)/lib/python\")" > $(RST2HTML); \
 
 %-isites.html: %.html $(LIBDIR)/html2isite.xsl $(TOPDIR)/bin/remove-bad-ents.pl
 	$(XSLTPROC) $(XSLTPROC_ISITES_FLAGS) $(LIBDIR)/html2isite.xsl $< | $(TOPDIR)/bin/remove-bad-ents.pl > $@
+
+%-screen.html: %.html $(LIBDIR)/htmlscreen.xsl 
+	$(XSLTPROC) $(LIBDIR)/htmlscreen.xsl $< > $@
 
 %.pdf: %.html
 	wkpdf --source $< --output $@
