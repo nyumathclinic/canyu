0953377c168641093fe89ecef8950486b9bc9cb0
Matthew Leingang <leingang@nyu.edu>
2008-09-16 19:01:15 +0000

    My <meta> -> <link> hacks enabled

--- lib/rst.mk
+++ lib/rst.mk
@@ -4,19 +4,30 @@
 # $Date: 2007-10-12 06:59:39 -0400 (Fri, 12 Oct 2007) $
 # $Revision: 171 $
 
+PYTHON=python
+DUDIR=$(LIBDIR)/docutils-0.5
 
 RST2HTML=$(TOPDIR)/bin/rst2html.py
 RST2HTML_FLAGS=--embed-stylesheet \
 	--stylesheet-path=$(LIBDIR)/new.css \
 	--cloak-email-addresses
-PYTHON=python
-DUDIR=$(LIBDIR)/docutils-0.5
+
 RST2LATEX=$(TOPDIR)/bin/rst2latex.py
-RST2LATEX_FLAGS=--use-latex-toc \
-        --use-latex-docinfo
+RST2LATEX_FLAGS=--use-latex-docinfo \
+	--documentoptions="10pt" \
 	--stylesheet=$(LIBDIR)/rst2latex.tex 
+
+RST2XML=$(TOPDIR)/bin/rst2xml.py
+RST2XML_FLAGS=
+
+
+
 XSLTPROC=xsltproc
-XSLTPROC_ISITES_FLAGS=
+XSLTPROC_SCREEN_FLAGS=--path "."
+
+# this is Mac OS X utility that runs on the command line and converts
+# html to pdf
+WKPDF=$(TOPDIR)/bin/wkpdf
 
 $(RST2HTML): $(LIBDIR)/rst2html.py
 	cd $(DUDIR) && $(PYTHON) setup.py install --home=$(DUDIR);\
@@ -36,15 +47,26 @@ sys.path.append('$(shell cd $(DUDIR) && pwd)/lib/python')" > $(RST2LATEX); \
 	cat $(LIBDIR)/rst2latex.py >> $(RST2LATEX)
 	chmod 701 $(RST2LATEX)
 
+$(RST2XML): $(DUDIR)/bin/rst2xml.py
+	cd $(DUDIR) && $(PYTHON) setup.py install --home=$(DUDIR);\
+	echo "#!/usr/bin/env python\n\
+import sys\n\
+sys.path.append('$(shell cd $(LIBDIR) && pwd)')\n\
+sys.path.append('$(shell cd $(DUDIR) && pwd)/lib/python')" > $(RST2XML); \
+	cat $(DUDIR)/bin/rst2xml.py >> $(RST2XML)
+	chmod 701 $(RST2XML)
 
-%.html: %.txt $(RST2HTML)
-	$(RST2HTML) $(RST2HTML_FLAGS) $< $@
 
-%-isites.html: %.html $(LIBDIR)/html2isite.xsl $(TOPDIR)/bin/remove-bad-ents.pl
-	$(XSLTPROC) $(XSLTPROC_ISITES_FLAGS) $(LIBDIR)/html2isite.xsl $< | $(TOPDIR)/bin/remove-bad-ents.pl > $@
+
+%.html: %.txt $(RST2HTML) $(LIBDIR)/rsthtmlfix.xsl
+	$(RST2HTML) $(RST2HTML_FLAGS) $< \
+	  | $(XSLTPROC) $(LIBDIR)/rsthtmlfix.xsl - > $@
 
 %-screen.html: %.html $(LIBDIR)/htmlscreen.xsl 
-	$(XSLTPROC) $(LIBDIR)/htmlscreen.xsl $< > $@
+	$(XSLTPROC) $(XSLTPROC_SCREEN_FLAGS) $(LIBDIR)/htmlscreen.xsl $< > $@
 
 %.tex: %.txt $(RST2LATEX)
 	$(RST2LATEX) $(RST2LATEX_FLAGS) $< $@
+
+%.xml: %.txt $(RST2XML)
+	$(RST2XML) $(RST2XML_FLAGS) $< $@
