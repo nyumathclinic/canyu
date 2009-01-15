6172a425160467ba63862d0114691cc7a556c0a7
Matthew Leingang <leingang@nyu.edu>
2009-01-15 04:38:08 +0000

    emacs commonds to transform LaTeX into RST

---
+++ docs/123/staff/import.el
@@ -0,0 +1,30 @@
+(defun latex-to-rst 
+        () 
+        "convert some included latex to restructured text"
+        (let ()
+	  (item-to-bullet)
+	  (fix-em)
+	  (fix-display-math)
+	  (fix-vector-abbreviations)
+	  ))
+
+(defun item-to-bullet () "convert \\items to bullets"
+  (replace-regexp "\\\\item\n?" "* "))
+
+
+(defun fix-em () "convert {\em ... } to *...*"
+  (replace-regexp "{\\\\em \\([^}]*\\)\\\\/}" "*\\1*"))
+
+
+(defun fix-display-math
+  ()
+  "convert displayed math to the latex directive"
+  (replace-regexp "\\$\\$\n?\\([^$]+\\)\n\\$\\$"
+		  "\n.. latex::\n\n   \\1\n"
+		  ))
+
+(defun fix-vector-abbreviations
+  ()
+  "undo some of Tom's macros"
+  (replace-regexp "\\\\bf\\([ijk]\\)" "\\\\mathbf{\\1}"))
+
